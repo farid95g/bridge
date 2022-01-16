1ad6b7d1b09d7c7c77207c570698a3200e60ae4b
farid95g <f95guluzade@gmail.com>
2022-01-16 22:10:10 +0400

    rule to compile scss files

--- webpack.config.js
+++ webpack.config.js
@@ -18,15 +18,18 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: [
+              "@babel/preset-env", "@babel/preset-react"
+            ],
           },
         },
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
           "style-loader",
-          "css-loader"
+          "css-loader",
+          "sass-loader"
         ]
       }
     ],
