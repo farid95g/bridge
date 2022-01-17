7d0b8d2e3f48f4b0550c4ae63ed735d23c46c691
farid95g <f95guluzade@gmail.com>
2022-01-17 21:58:28 +0400

    homepage simple markup

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,8 +8,23 @@ const Homepage = (props) => {
 
   return (
     <div className='homepage'>
+      <div className='balance'>
+        <span>Balance: {'71,429'}</span>
+      </div>
       <h1>Кто выйграет?</h1>
       <span>Сыграй в игру и испытай удачу</span>
+      <div className='game-zone'>
+        <div>
+          <div>
+            <div className='card'>?</div>
+            <button>Слева</button>
+          </div>
+          <div>
+            <button>Справа</button>
+            <div className='card'>?</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { authReducer } from './reducers/auth';
+import { gameReducer } from './reducers/game';
 
 const reducers = combineReducers({
   authReducer,
+  gameReducer
 });
 
 export const store = createStore(reducers, applyMiddleware(thunk));
