baa5ab2a43338e36b4654d61af498512e0f4083d
farid95g <f95guluzade@gmail.com>
2022-01-20 14:54:23 +0400

    accept username with upper or lower case in login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,7 +2,7 @@ import { authData } from '../data/authData';
 
 export const authServices = {
   login(username, password) {
-    if (username === authData.username && password === authData.password) {
+    if (username.toLowerCase() === authData.username.toLowerCase() && password === authData.password) {
       return true;
     }
     return false;
